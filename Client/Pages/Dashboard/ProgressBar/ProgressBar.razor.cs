fb7b054fb27483f2e573472ae71908fcc4956410
Matthew Eskolin <matteskolin@gmail.com>
2022-03-06 08:36:19 -0600

    cleanup

---
+++ Client/Pages/Dashboard/ProgressBar/ProgressBar.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TogglTimeWeb.Client.Pages.Dashboard.ProgressBar
+{
+    public partial class Budget
+    {
+        [Parameter]
+        public string 
+        [Parameter]
+        [Parameter]
+
+
+
+    }
+}
---
+++ Client/Pages/TimeGoals/Budget.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TogglTimeWeb.Client.Pages.TimeGoals
+{
+    public partial class Budget
+    {
+        //[Parameter]
+        //public string 
+        //[Parameter]
+        //[Parameter]
+
+
+
+    }
+}
